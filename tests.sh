f4b217dc87a8ba23b460fd5f01b0b6e14eca68e0
nu11p01n73R <kjjohny1991@gmail.com>
2016-06-04 18:18:04 +0530

    test scripts

---
+++ tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./connect.sh
+
+# Test reading config file.
+read_config_file
+echo "Config file read successfully"
+
+
+# Prints the read config file 
+echo "Server Names"
+echo "==============="
+for name in $names
+do
+    echo -e "\tName: \t$name"
+    echo -e "\tKey: \t${keys[$name]}"
+    echo -e "\tUser: \t${usernames[$name]}"
+    echo -e "\tIp: \t${ips[$name]}"
+    echo "==============="
+done
