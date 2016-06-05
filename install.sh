3397e751a8e904c02d04692de7324431e50abd1b
nu11p01n73R <kjjohny1991@gmail.com>
2016-06-05 20:01:21 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Install script for connect.
+# Creates a simbolic link from /usr/bin/connect to ./main.sh
+# Should have root permission to run the command.
+#
+
+#
+# Entry script.
+#
+FILE_NAME="main.sh"
+
+#
+# Error codes
+#
+PERM_DENAID=50
+
+if [[ "$EUID" -ne 0 ]]
+then
+    echo "Permission denaid : Run the script as root"
+    exit $PERM_DENAID
+fi
+
+echo "Creating link /usr/bin/connect to $(pwd)/$FILE_NAME"
+ln -s "$(pwd)/$FILE_NAME" /usr/bin/connect
