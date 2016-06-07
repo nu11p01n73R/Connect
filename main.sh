11f1587becab95e9515e1f5ed062060f47fde038
nu11p01n73R <kjjohny1991@gmail.com>
2016-06-07 23:04:44 +0530

    associative array fix

--- main.sh
+++ main.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 #
 # Entry script to the connect command.
 #
 
 SOURCE_FILE="./connect.sh"
+pwd
 
 source $SOURCE_FILE
 
 sanity_checks
 read_config_file
+
 connect $1
