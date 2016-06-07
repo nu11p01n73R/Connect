fa64ebc50b697e4ca4d7ea14333033e6c816ef96
nu11p01n73R <kjjohny1991@gmail.com>
2016-06-08 00:07:03 +0530

    directory fix

--- main.sh
+++ main.sh
@@ -1,13 +1,34 @@
 #!/usr/local/bin/bash
 
 #
+# The CONNECT_PATH variable must be set correctly
+# for the connect to work. This variable holds the
+# path to the Connect directory.
+#
+if [[ -z "$CONNECT_PATH" ]]
+then
+    echo "CONNECT_PATH is not set"
+    echo "Run export CONNECT_PATH=path"
+    exit 10 
+fi
+
+#
 # Entry script to the connect command.
 #
+SOURCE_FILE="${CONNECT_PATH}connect.sh"
 
-SOURCE_FILE="./connect.sh"
-pwd
 
-source $SOURCE_FILE
+#
+# Checks if the connect.sh exists in the
+# CONNECT_PATH
+#
+if [[ ! -f "$SOURCE_FILE" ]]
+then
+    echo "Cannot find source file at $CONNECT_PATH"
+    exit 11
+fi
+
+source "$SOURCE_FILE"
 
 sanity_checks
 read_config_file
